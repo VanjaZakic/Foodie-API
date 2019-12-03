fc7aac0e63378565d45bbfd758e35ee4c85bf11c
Vanja Zakic <vanja.zakic@quantox.com>
2019-12-03 16:41:42 +0100

    proba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing
+                    Proba
                 </div>
 
                 <div class="links">
