694f38c8ae387cea7c80001e171216e6d2bd9969
Vanja Zakic <vanja.zakic@quantox.com>
2020-01-08 10:28:29 +0100

    [FO-10] chore: create(modify) migrations

---
+++ database/migrations/2019_12_31_165745_changeColumn_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeColumnOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->enum('status', ['ordered', 'cancelled', 'processing', 'deliverded'])->default('ordered');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->enum('status', ['ordered', 'cancelled', 'processing', 'deliverded']);
+        });
+    }
+}
