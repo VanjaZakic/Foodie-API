bb0e13ad58e97d6a519f9b18933c9d9996c63f93
Sanja Mitrovic <sanja.mitrovic@quantox.com>
2020-01-09 15:40:41 +0100

    [FO] feat: error handler

---
+++ app/Exceptions/AdminUserForCompanyAlreadyExistsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Class AdminUserForCompanyAlreadyExistsException
+ * @package App\Exceptions
+ */
+class AdminUserForCompanyAlreadyExistsException extends Exception
+{
+    //
+}
---
+++ app/Exceptions/InvalidUserRoleForCompanyException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvalidUserRoleForCompanyException
+ * @package App\Exceptions
+ */
+class InvalidUserRoleForCompanyException extends Exception
+{
+    //
+}
