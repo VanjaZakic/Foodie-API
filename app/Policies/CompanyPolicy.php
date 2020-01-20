103e22d86dff86ac14cc5e469bb0f64ec946449e
Sanja Mitrovic <sanja.mitrovic@quantox.com>
2020-01-20 15:45:26 +0100

    [FO-6] fix: Create CompanyPolicy

---
+++ app/Policies/CompanyPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Company;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CompanyPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    public function view(User $user, Company $company)
+    {
+        return $user->company_id === $company->id;
+    }
+}
